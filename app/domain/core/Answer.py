9ae14346911d72daaa5fe3f860d3bb345826b983
Mattia Matteini <matteinimattia@gmail.com>
2024-03-19 21:54:29 +0100

    refactor(Answer): Answer is now a dataclass

--- app/domain/core/Answer.py
+++ app/domain/core/Answer.py
@@ -1,22 +1,11 @@
+from dataclasses import dataclass
+
+
+@dataclass
 class Answer:
 
-    def __init__(self, text: str, value: str):
-        self._text = text
-        self._value = value
-
-    @property
-    def text(self):
-        return self._text
-
-    @property
-    def value(self):
-        return self._value
-
-    def __str__(self):
-        return f"Answer(text={self.text}, value={self.value})"
+    text: str
+    value: str
 
     def __hash__(self):
         return hash((self.text, self.value))
-
-    def __eq__(self, other):
-        return self.text == other.text and self.value == other.value
