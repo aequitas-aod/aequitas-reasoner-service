af678664ec1b916663fb4b57bac7b0fe19e94e16
Mattia Matteini <matteinimattia@gmail.com>
2024-03-19 21:36:39 +0100

    chore: add presentation package and answers in answer resource

--- app/resources/answers.py
+++ app/resources/answers.py
@@ -1,8 +1,20 @@
 from flask import Blueprint
 from flask_restful import Api, Resource
 
+from app.domain.factories.AnswerFactory import AnswerFactory
+
 answers_bp = Blueprint("answers", __name__)
 api = Api(answers_bp)
+
+answer_factory = AnswerFactory()
+answers = {
+    answer_factory.create_answer("Always", "always"),
+    answer_factory.create_answer("Usually", "usually"),
+    answer_factory.create_answer("Sometimes", "sometimes"),
+    answer_factory.create_answer("Never", "never"),
+    answer_factory.create_boolean_answer(True),
+    answer_factory.create_boolean_answer(False),
+}
 
 
 class AnswerResource(Resource):
