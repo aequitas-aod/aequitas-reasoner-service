3693279713aa7d12185927da698a6d3c391eca87
Mattia Matteini <matteinimattia@gmail.com>
2024-04-08 14:01:54 +0200

    refactor: remove answers api spec, remove old serialization functions

--- app/presentation/presentation.py
+++ app/presentation/presentation.py
@@ -4,30 +4,6 @@ from pydantic import BaseModel
 import app.domain.core as _core_domain
 
 
-# def serialize_answer(answer: Answer) -> dict:
-#     return json.loads(answer.model_dump_json())
-
-
-# def deserialize_answer(answer: dict) -> Answer:
-#     return Answer(**answer)
-
-
-# def serialize_question(question: Question) -> dict:
-#     return json.loads(question.model_dump_json())
-
-
-# def deserialize_question(question: dict) -> Question:
-#     return Question(**question)
-
-
-# def serialize_question_id(question_id: QuestionId) -> dict:
-#     return json.loads(question_id.model_dump_json())
-
-
-# def deserialize_question_id(question_id: dict) -> QuestionId:
-#     return QuestionId(**question_id)
-
-
 def _is_admissible_type(obj: type) -> bool:
     for name in dir(_core_domain):
         symbol = getattr(_core_domain, name)
