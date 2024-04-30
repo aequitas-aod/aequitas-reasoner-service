bd743181828b4033f878ea909b10ed57320b56c6
Mattia Matteini <matteinimattia@gmail.com>
2024-04-30 17:33:34 +0200

    refactor: remove .env.test

--- utils/env.py
+++ utils/env.py
@@ -2,10 +2,7 @@ import os
 
 from dotenv import load_dotenv
 
-if os.environ["TEST"] == "true":
-    load_dotenv(".env.test")
-else:
-    load_dotenv(".env")
+load_dotenv()
 
 DB_HOST = os.environ.get("DB_HOST")
 DB_USER = os.environ.get("DB_USER")
