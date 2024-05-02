cc2a4c6107e2d0a096e864ba3aa1b6d9736d0c57
Mattia Matteini <matteinimattia@gmail.com>
2024-05-02 16:29:17 +0200

    build: create poe tasks

--- utils/env.py
+++ utils/env.py
@@ -3,13 +3,13 @@ import os
 from dotenv import load_dotenv, find_dotenv
 
 shell_env = os.environ.copy()
-load_dotenv(find_dotenv('.env'), override=True)
+load_dotenv(find_dotenv(".env"), override=True)
 prod_env = os.environ.copy()
 
 ENV = shell_env.get("ENV") or prod_env.get("ENV")
 
 if ENV == "test":
-    load_dotenv(find_dotenv('.env.test'), override=True)
+    load_dotenv(find_dotenv(".env.test"), override=True)
     DB_HOST = os.environ.get("DB_HOST")
     DB_USER = os.environ.get("DB_USER")
     DB_PASSWORD = os.environ.get("DB_PASSWORD")
