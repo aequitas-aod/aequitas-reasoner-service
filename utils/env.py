04564a58a1a8e1927c2e23593a3fc04a7c368660
Mattia Matteini <matteinimattia@gmail.com>
2024-05-02 11:05:05 +0200

    fix: getting env variables

--- utils/env.py
+++ utils/env.py
@@ -1,8 +1,9 @@
 import os
+import sys
 
 from dotenv import load_dotenv
 
-load_dotenv()
+load_dotenv(sys.path[1] + '/.env')
 
 DB_HOST = os.environ.get("DB_HOST")
 DB_USER = os.environ.get("DB_USER")
