e7e917c927d827741fdd5caf5285967c29c1a23f
Mattia Matteini <matteinimattia@gmail.com>
2024-05-02 15:22:35 +0200

    refactor: get env vars with priority and fallbacks

--- utils/env.py
+++ utils/env.py
@@ -2,11 +2,18 @@ import os
 
 from dotenv import load_dotenv, find_dotenv
 
-if os.environ.get("TEST") == "true":
-    load_dotenv(find_dotenv(".env.test"))
-else:
-    load_dotenv(find_dotenv(".env"))
+shell_env = os.environ.copy()
+load_dotenv(find_dotenv('.env'), override=True)
+prod_env = os.environ.copy()
 
-DB_HOST = os.environ.get("DB_HOST")
-DB_USER = os.environ.get("DB_USER")
-DB_PASSWORD = os.environ.get("DB_PASSWORD")
+ENV = shell_env.get("ENV") or prod_env.get("ENV")
+
+if ENV == "test":
+    load_dotenv(find_dotenv('.env.test'), override=True)
+    DB_HOST = os.environ.get("DB_HOST")
+    DB_USER = os.environ.get("DB_USER")
+    DB_PASSWORD = os.environ.get("DB_PASSWORD")
+else:
+    DB_HOST = shell_env.get("DB_HOST") or prod_env.get("DB_HOST")
+    DB_USER = shell_env.get("DB_USER") or prod_env.get("DB_USER")
+    DB_PASSWORD = shell_env.get("DB_PASSWORD") or prod_env.get("DB_PASSWORD")
