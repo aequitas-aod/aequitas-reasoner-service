d4c9cd848783e2a580b5f31875a6343de96f8396
Mattia Matteini <matteinimattia@gmail.com>
2024-05-13 15:55:08 +0200

    chore: add cors

--- ws/main.py
+++ ws/main.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_cors import CORS
 
 from ws.resources.projects import projects_bp
 from ws.resources.questionnaires import questionnaires_bp
@@ -7,6 +8,7 @@ from ws.resources.questions import questions_bp
 
 def create_app():
     app = Flask(__name__)
+    CORS(app)
     app.register_blueprint(questions_bp)
     app.register_blueprint(projects_bp)
     app.register_blueprint(questionnaires_bp)
