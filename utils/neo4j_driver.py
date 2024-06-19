03f0268df27f097bde846dba49e687662f6ea7af
Mattia Matteini <matteinimattia@gmail.com>
2024-06-19 12:28:20 +0200

    refactor(utils): add Neo4jQuery

--- utils/neo4j_driver.py
+++ utils/neo4j_driver.py
@@ -1,5 +1,5 @@
-import time
-from typing import LiteralString
+from time import sleep
+from typing import List
 
 from neo4j import GraphDatabase, Driver, Result
 from neo4j.exceptions import ServiceUnavailable
@@ -9,6 +9,12 @@ class Credentials:
     def __init__(self, user: str, password: str):
         self.user = user
         self.password = password
+
+
+class Neo4jQuery:
+    def __init__(self, query: str, params: dict):
+        self.query = query
+        self.params = params
 
 
 class Neo4jDriver:
@@ -22,16 +28,16 @@ class Neo4jDriver:
             auth=(credentials.user, credentials.password),
         )
 
-    def query(self, query: str, **kwargs: dict) -> Result:
-        for attempt in range(self.retries):
-            try:
-                with self.driver.session() as session:
-                    return session.run(query, **kwargs)
-            except ServiceUnavailable as e:
-                if attempt < self.retries - 1:
-                    time.sleep(self.delay)
-                else:
-                    raise e
+    def query(self, query: Neo4jQuery) -> Result:
+        with self.driver.session() as session:
+            return session.run(query.query, query.params)
+
+    def transaction(self, queries: List[Neo4jQuery]) -> None:
+        with self.driver.session() as session:
+            with session.begin_transaction() as tx:
+                for query in queries:
+                    tx.run(query.query, query.params)
+                tx.commit()
 
     def close(self):
         self.driver.close()
